e2d27ed3c9fec58e735972945a1b4fa464f12a08
Caleb Hyde <caleb.hyde@gmail.com>
2020-09-24 14:07:43 -0700

    Add BNO055 IMU logic and update reader testing

--- arduino/quadruped.ino
+++ arduino/quadruped.ino
@@ -1,4 +1,12 @@
 #include <Servo.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
+
+#define RELAY_ENABLED 0
+#define IMU_ENABLED 1
+#define NUM_SERVOS 12
 
 // https://roboticsbackend.com/raspberry-pi-arduino-serial-communication/#Bidirectional_Serial_communication_between_Raspberry_Pi_and_Arduino
 unsigned long loopTime;
@@ -6,15 +14,127 @@ unsigned long previousMillis = 0;
 const long interval = 20;
 const String startMarker = "<";
 const String endMarker = ">";
+const int RELAY_PIN = 3;
 
-#define NUM_SERVOS 12
 Servo Servos[NUM_SERVOS];
 int pulses[NUM_SERVOS];
 int i = 0;
 
+Adafruit_BNO055 bno;
+
+String readIMU()
+{
+    /* Get BNO-055 IMU data
+    
+    Based off of https://github.com/adafruit/Adafruit_BNO055/blob/master/examples/webserial_3d/webserial_3d.ino
+    Board layout:
+         +----------+
+         |         *| RST   PITCH  ROLL  HEADING
+     ADR |*        *| SCL
+     INT |*        *| SDA     ^            /->
+     PS1 |*        *| GND     |            |
+     PS0 |*        *| 3VO     Y    Z-->    \-X
+         |         *| VIN
+         +----------+
+    */
+
+    sensors_event_t event;
+    bno.getEvent(&event);
+
+    float ex, ey, ez, qx, qy, qz, qw;
+
+    ex = event.orientation.x;
+    ey = event.orientation.y;
+    ez = event.orientation.z;
+
+    imu::Quaternion quat = bno.getQuat();
+    qx = quat.x();
+    qy = quat.y();
+    qz = quat.z();
+    qw = quat.w();
+
+    /* Also send calibration data for each sensor. */
+    // uint8_t sys, gyro, accel, mag = 0;
+    // response.concat(sys, DEC);
+    // response.concat(gyro, DEC);
+    // response.concat(accel, DEC);
+    // response.concat(mag, DEC);
+
+    // "BNO055~%s %s %s %s %s %s %s",
+    // where each %s is ffmt(ex, 4,2) = 6 char plus the decimal place, plus the "#" character separator == 8 char
+    // "BNO055~".length() == 7, ffmt * six vars = 48
+    // resp_len = 7+ 48 = 55 (but maybe we should still pad this?)
+    // int resp_len = sizeof(ex) + sizeof(ey) + ...;
+    int resp_len = 80;
+    char response[resp_len];
+    int width = 4;
+    int precision = 2;
+
+    char fex[width + precision + 1];
+    dtostrf(ex, width, precision, fex);
+
+    char fey[width + precision + 1];
+    dtostrf(ey, width, precision, fey);
+
+    char fez[width + precision + 1];
+    dtostrf(ez, width, precision, fez);
+
+    char fqx[sizeof(qx)];
+    dtostrf(qx, width, precision, fqx);
+
+    char fqy[width + precision + 1];
+    dtostrf(qy, width, precision, fqy);
+
+    char fqz[width + precision + 1];
+    dtostrf(qz, width, precision, fqz);
+
+    char fqw[width + precision + 1];
+    dtostrf(qw, width, precision, fqw);
+
+    sprintf(response, "BNO055~%s %s %s %s %s %s %s", fex, fey, fez, fqx, fqy, fqz, fqw);
+    return response;
+}
+
+void setup_imu()
+{
+    bno = Adafruit_BNO055(55, 0x28);
+    if (!bno.begin())
+    {
+        /* There was a problem detecting the BNO055 ... check your connections */
+        Serial.println("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+    }
+    bno.setExtCrystalUse(true);
+}
+
+void relay(int state = 0)
+{
+    if (state == 0)
+    {
+        digitalWrite(RELAY_PIN, LOW);
+    }
+    else
+    {
+        digitalWrite(RELAY_PIN, HIGH);
+    }
+}
+
 void setup()
 {
+    if (IMU_ENABLED)
+    {
+        setup_imu();
+    }
+    if (RELAY_ENABLED)
+    {
+        // Relay
+        pinMode(RELAY_PIN, OUTPUT);
+    }
+
     Serial.begin(115200);
+    while (!Serial)
+    {
+        delay(interval);
+    }
     {
         for (i = 0; i < NUM_SERVOS; i++)
         {
@@ -34,6 +154,10 @@ String shutdown()
 
 String moveServos(String instructions)
 {
+    /*
+        What println(instructions) looks like:
+        #0~1239#1~1893#2~397#3~1570#4~439#5~1887#6~1484#7~379#8~658#9~871#10~1202#11~786
+    */
     String wordSep = "#";
     String movSep = "~";
     String word = "";
@@ -41,13 +165,11 @@ String moveServos(String instructions)
     int spc = 1;
     String response = "FAIL";
 
-    // Serial.println(instructions);
-    // #0~1239#1~1893#2~397#3~1570#4~439#5~1887#6~1484#7~379#8~658#9~871#10~1202#11~786
     if (NUM_SERVOS == 0)
     {
         return "No servos configured";
     }
-    response = "";
+    response = "SERVOS~";
 
     while (wordIdx < NUM_SERVOS && spc > 0)
     {
@@ -58,7 +180,7 @@ String moveServos(String instructions)
         int servo = word.substring(0, movIdx).toInt();
         String distance = word.substring(movIdx + 1);
 
-        response.concat(" * ");
+        response.concat(" ");
         response.concat(servo);
         response.concat("->");
         response.concat(distance);
@@ -108,9 +230,9 @@ void loop()
     if (Serial.available() > 0)
     {
         String data = Serial.readStringUntil('\n');
-
         if (data.startsWith(startMarker) && data.indexOf(endMarker) > 0)
         {
+
             // The data we read is a control string, "<...>"
             String control = data.substring(1, data.length() - 1);
             String response = "<";
@@ -121,10 +243,9 @@ void loop()
         else
         {
             // Data received is not control, so just echo whatever it is
-            // Serial.print("You sent: ");
-            // Serial.println(data);
+            Serial.print("You sent: ");
+            Serial.println(data);
         }
+        Serial.println(readIMU());
     }
-
-    // When we have IMU data, send that as a "<DATA...>" record
 }
--- arduino/read_serial.py
+++ arduino/read_serial.py
@@ -11,10 +11,10 @@ from random import randint, random
 import logging
 import numpy as np
 
-logging.basicConfig(level=logging.WARNING)
+logging.basicConfig(level=logging.INFO)
 
 loopTime = 0.0
-interval = 0.01
+interval = 0.5
 freqMulti = 2
 speed = 10
 
@@ -24,29 +24,53 @@ HEADERS = [
     "LCD",
 ]
 
+MOVING = False
+
 rad2pulse = lambda x: int(-speed * np.rad2deg(-x)) + 1500
+
+
+def parse(record):
+    try:
+        lines = record.decode("utf-8").strip()
+    except:
+        lines = record
+    try:
+        lines = [l.strip() for l in lines.split("\n")]
+    except:
+        lines = [lines]
+    for line in lines:
+        try:
+            header, message = line.split("~")
+        except:
+            header = ""
+            message = line
+        logging.info(f"Received [{header}]: {message}")
+    return lines
+
 
 if __name__ == "__main__":
     ser = serial.Serial("/dev/ttyACM0", 115200, timeout=1)
     ser.flush()
 
     while True:
-        if time.time() - loopTime >= interval:
-            loopTime = time.time()
+        if time.time() - loopTime < interval:
+            continue
+        loopTime = time.time()
 
-            moves = []
-            rad = np.sin(freqMulti * loopTime)
-            for i in range(12):
-                # dist = rad2pulse(rad)
+        # TODO: https://github.com/pyserial/pyserial/blob/master/serial/tools/miniterm.py#L499
+        record = ser.read(ser.in_waiting or 1)
+        parse(record)
+
+        moves = []
+        rad = np.sin(freqMulti * loopTime)
+        for i in range(12):
+            if MOVING:
+                dist = rad2pulse(rad)
+            else:
                 dist = 90 + np.rad2deg(rad)
-                moves.append(f"{i}~{dist}")
-            message = "<SERVO#{}>\n".format("#".join(moves))
-            logging.info(f"Sending: {message}")
-        else:
-            message = "Hello from Raspberry Pi!\n"
+            moves.append(f"{i}~{dist}")
+        message = "<SERVO#{}>\n".format("#".join(moves))
 
+        # logging.info(f"Sending: {message}")
+        # TODO: Why does this block readIMU ?
         ser.write(bytes(message, "utf-8"))
-
-        line = ser.readline().decode("utf-8").rstrip()
-        if line:
-            logging.info(line)
